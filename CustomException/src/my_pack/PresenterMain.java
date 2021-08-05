225eba685687f36a2b384628ecf437bd4a7cd1b0
Rutuja Kasar <rutujakasar41999@gmail.com>
2021-08-05 08:32:29 +0530

    Exception Handling Task

---
+++ CustomException/src/my_pack/PresenterMain.java
@@ -0,0 +1,16 @@
+package my_pack;
+
+public class PresenterMain {
+	public static void main(String[]args) {
+		
+		BusinessValidation b1=new BusinessValidation();
+		try {
+			if(b1.isValidPassword("ABCdef45*")) {
+				System.out.println("Pan Validated");
+			}
+			}catch(InvalidPassException e){
+				System.out.println(e.getMessage());
+			}
+
+	}
+}
